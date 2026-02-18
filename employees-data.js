7c257c1c71dafdb679d5df5d93ae9fb0ee6e246b
krishnagiriai-source <krishnagiri.ai@gmail.com>
2026-02-18 12:50:08 +0530

    Auto-publish employees (2026-02-18T07:20:08.648Z)

--- employees-data.js
+++ employees-data.js
@@ -1,5 +1,5 @@
 // MRK Foods - Employee Data File
-// Auto-published: 2026-02-18T07:09:21.664Z
+// Auto-published: 2026-02-18T07:20:08.648Z
 window.MRK_EMPLOYEES = [
   {
     "id": "emp1771398484309",
@@ -17,5 +17,22 @@ window.MRK_EMPLOYEES = [
     "photo": null,
     "logo": null,
     "createdAt": "2026-02-18T07:08:04.317Z"
+  },
+  {
+    "id": "emp1771399202201",
+    "name": "Vijay Vijayrajan",
+    "designation": "Branch Manager (Delhi)",
+    "mobile": "9820595083",
+    "email": "northindia@mrkfoods.in",
+    "website": "https://www.mrkfoods.in/",
+    "address": "Ghitorni village, Road No. 01, Plot No. 19/C, Opposite Gropher Warehouse New Delhi - 110030",
+    "whatsapp": "+91 9820595083",
+    "facebook": "https://www.facebook.com/mrkfoodspvtltd/",
+    "linkedin": "https://wa.me/919820595083",
+    "instagram": "https://www.instagram.com/mrkfoodspvtltd/?hl=en",
+    "catalogueLink": "MRK_Retail Catalogue.pdf",
+    "photo": null,
+    "logo": null,
+    "createdAt": "2026-02-18T07:20:02.202Z"
   }
 ];
