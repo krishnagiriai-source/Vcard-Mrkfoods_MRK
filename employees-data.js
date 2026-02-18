cd63f4f6f2b3508d1386614ea374f0e8736a61b6
krishnagiriai-source <krishnagiri.ai@gmail.com>
2026-02-18 15:04:28 +0530

    Auto-publish employees (2026-02-18T09:34:28.027Z)

--- employees-data.js
+++ employees-data.js
@@ -1,5 +1,5 @@
 // MRK Foods - Employee Data File
-// Auto-published: 2026-02-18T07:55:44.124Z
+// Auto-published: 2026-02-18T09:34:28.027Z
 window.MRK_EMPLOYEES = [
   {
     "id": "emp1771398484309",
@@ -102,5 +102,22 @@ window.MRK_EMPLOYEES = [
     "photo": null,
     "logo": null,
     "createdAt": "2026-02-18T07:55:36.262Z"
+  },
+  {
+    "id": "emp1771407253030",
+    "name": "Sheetal Suresh Jandial",
+    "designation": "Marketing Consultant",
+    "mobile": "9594723985",
+    "email": "sheetal@mrkfoods.in",
+    "website": "https://www.mrkfoods.in/",
+    "address": "",
+    "whatsapp": "+919594723985",
+    "facebook": "https://www.facebook.com/mrkfoodspvtltd/",
+    "linkedin": "https://wa.me/919594723985",
+    "instagram": "https://www.instagram.com/mrkfoodspvtltd/?hl=en",
+    "catalogueLink": "MRK_Retail Catalogue.pdf",
+    "photo": null,
+    "logo": null,
+    "createdAt": "2026-02-18T09:34:13.041Z"
   }
 ];
