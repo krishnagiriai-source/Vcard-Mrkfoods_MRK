ecefe652e522551fb411f1054f9a774cf29f3a28
krishnagiriai-source <krishnagiri.ai@gmail.com>
2026-02-18 13:25:44 +0530

    Auto-publish employees (2026-02-18T07:55:44.124Z)

--- employees-data.js
+++ employees-data.js
@@ -1,5 +1,5 @@
 // MRK Foods - Employee Data File
-// Auto-published: 2026-02-18T07:40:39.803Z
+// Auto-published: 2026-02-18T07:55:44.124Z
 window.MRK_EMPLOYEES = [
   {
     "id": "emp1771398484309",
@@ -85,5 +85,22 @@ window.MRK_EMPLOYEES = [
     "photo": null,
     "logo": null,
     "createdAt": "2026-02-18T07:40:33.432Z"
+  },
+  {
+    "id": "emp1771401336262",
+    "name": "Smita Gaurav Hajare",
+    "designation": "Sales Manager (Regional)",
+    "mobile": "9664360657",
+    "email": "dispatch@mrkfoods.in",
+    "website": "https://www.mrkfoods.in/",
+    "address": "Ground Floor,Vijay House, Devidayal Compound, LBS Road, Near Samruddhi Garden, Behind Apollo Pharmacy. Bhandup West-400078, Mumbai, Maharashtra.",
+    "whatsapp": "+91 9664360657",
+    "facebook": "https://www.facebook.com/mrkfoodspvtltd/",
+    "linkedin": "https://wa.me/919664360657",
+    "instagram": "https://www.instagram.com/mrkfoodspvtltd/?hl=en",
+    "catalogueLink": "MRK_Retail Catalogue.pdf",
+    "photo": null,
+    "logo": null,
+    "createdAt": "2026-02-18T07:55:36.262Z"
   }
 ];
